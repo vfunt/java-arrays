d41c96e5c10203afaf46671322623376c5767425
Valeriy Funt <valeriyfunt@gmail.com>
2019-07-02 09:18:01 +0300

    ArrayUtils class added

---
+++ src/main/java/ArrayUtils.java
@@ -0,0 +1,40 @@
+package main.java;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+public class ArrayUtils {
+    public static Number sumPositive(Number[] arrayNumbers) {
+        Number sum = 0;
+        BigDecimal sumBig = new BigDecimal(sum.doubleValue());
+        Integer z = 0;
+        for (int i = 0; i < arrayNumbers.length - 1; i++) {
+            BigDecimal bigDecimal = new BigDecimal(arrayNumbers[i].doubleValue());
+            BigDecimal zero = new BigDecimal(z.doubleValue());
+            if (bigDecimal.compareTo(zero) == 1) {
+                sumBig = sumBig.add(bigDecimal);//sumBig.add(bigDecimal);
+            }
+        }
+        return sumBig;
+    }
+
+    public static Number avg(Number[] array) {
+        BigDecimal avg = new BigDecimal(array[0].doubleValue());
+        Integer l = array.length;
+        BigDecimal length = new BigDecimal(l.doubleValue());
+        for (int i = 1; i < array.length - 1 ; i++) {
+            BigDecimal element = new BigDecimal(array[i].doubleValue());
+            avg = avg.add(element);
+        }
+        return avg.divide(length);
+    }
+
+    public static ArrayList<Integer> divisibleby(ArrayList<Integer> arrlist, int divby) {
+        for (int i = 0; i < arrlist.size(); i++) {
+            if (arrlist.get(i) % divby > 0) {
+                arrlist.remove(i);
+            }
+        }
+        return arrlist;
+    }
+}
